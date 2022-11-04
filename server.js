3d287873551aac9440111b1011662e8d16ed4181
AbahBrian <105516057+AbahBrian@users.noreply.github.com>
2022-11-05 05:49:49 +0700

    Delete Cors

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const dbconnection = require('./dbconnection')
 const app = express()
 const port = 5000
-const cors = require ('cors')
 const UserRouter = require('./routes/userRoute')
 const TransactionRouter = require('./routes/transactionRoute')
 const budgetRouter = require('./routes/budgetRoute')
@@ -17,7 +16,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/api', UserRouter)
 app.use('/api',TransactionRouter)
 app.use('/api',budgetRouter)
-app.use(cors())
 
 app.listen(port, () => {
     console.log(`Server is Listening on http://localhost:${port}`)
