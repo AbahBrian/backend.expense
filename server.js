bad542865bfe17ab10f02af900c4402fc6d08e64
AbahBrian <105516057+AbahBrian@users.noreply.github.com>
2022-11-05 05:54:20 +0700

    Update CORS

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dbconnection = require('./dbconnection')
 const app = express()
 const port = 5000
+const cors = require ('cors')
 const UserRouter = require('./routes/userRoute')
 const TransactionRouter = require('./routes/transactionRoute')
 const budgetRouter = require('./routes/budgetRoute')
@@ -16,6 +17,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/api', UserRouter)
 app.use('/api',TransactionRouter)
 app.use('/api',budgetRouter)
+app.use(cors(({
+    origin:'http//localhost:3000'
+})))
 
 app.listen(port, () => {
     console.log(`Server is Listening on http://localhost:${port}`)
